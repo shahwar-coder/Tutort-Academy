02fd660f536c04446da6dae9085ae70dd5184e91
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-09-21 13:51:47 +0530

    Create 09_boolean_print.py

---
+++ L02-Introduction-2/09_boolean_print.py
@@ -0,0 +1,11 @@
+'''
+Q. Take a boolean and print:
+'''
+boolean_value=input("Enter a Boolean Value: ")
+
+if boolean_value=="True":
+  print("Boolean Value is : ", True)
+elif boolean_value=="False":
+  print("Boolean Value is : ", False)
+else:
+  print("Not Boolean!")
