5e334f2b13bd1684fa4523907ce39aca2f8d1ffe
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-09-21 21:16:45 +0530

    Update 10_boolean_print.py

--- L02-Introduction-2/10_boolean_print.py
+++ L02-Introduction-2/10_boolean_print.py
@@ -1,12 +1,15 @@
 '''
 Q. Take a boolean and print:
 '''
-bool_book={"1": True, 
+bool_map={"1": True, 
            "0": False, 
            "true": True, 
            "false": False}
-bool_value = input("Enter a Boolean Value: ").strip().lower()
-result = bool_book.get(bool_value, None)
+
+user_input = input("Enter a Boolean Value: ").strip().lower()
+
+result = bool_map.get(user_input, None)
+
 if result is not None:
   print("Your Boolean Value is : ", result)
 else:
