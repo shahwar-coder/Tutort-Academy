0fe6e0a8dded3bda01da97b1ac35ea9c4ffd8551
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-09-23 20:27:02 +0530

    Update assignment_01.py

--- L02-Introduction-2/assignment_01.py
+++ L02-Introduction-2/assignment_01.py
@@ -2,7 +2,7 @@
 Problem 1 : Find Minimum of three numbers
 '''
 from typing import Sequence
-def find_minimum(numbers: Sequence[float|int])->float:
+def find_minimum(numbers: Sequence[float|int])->float: #or Sequence[Union[float, int]
   """
   Find and return the minimum of exactly 3 numbers.
   Raises ValueError if input does not contain 3 elements.
