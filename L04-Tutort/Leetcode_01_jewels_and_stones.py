e4b0eb1f7b3cb512a4bfdabbc05cf8565e6381b4
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-10 14:51:23 +0530

    Create Leetcode_01_jewels_and_stones.py

---
+++ L04-Tutort/Leetcode_01_jewels_and_stones.py
@@ -0,0 +1,12 @@
+'''
+https://leetcode.com/problems/jewels-and-stones/description/
+'''
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count=0
+        for stone in stones:
+            for jewel in jewels:
+                if stone == jewel:
+                    count+=1
+        return count
