40671c657de136b56d8d3bb08b33b5d96babaca3
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-12 13:27:59 +0530

    Update Leetcode_03_add_digits.py

--- L04-Tutort/Leetcode_03_add_digits.py
+++ L04-Tutort/Leetcode_03_add_digits.py
@@ -8,3 +8,10 @@ class Solution:
         while num>=10:
             num=sum(map(int, list(str(num))))
         return num
+
+# num = sum(int(d) for d in str(num)) # can be used as well
+# To make it faster : mathematical sum approach : 
+
+# class Solution:
+#     def addDigits(self, num: int) -> int:
+#         return 0 if num == 0 else 1 + (num - 1) % 9
