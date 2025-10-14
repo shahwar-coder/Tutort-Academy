d406f1b9aba77b258505732f5c34effaa0f8eef9
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-14 12:32:52 +0530

    Update Leetcode_02_baseball_game.py

--- L04-Tutort/Leetcode_02_baseball_game.py
+++ L04-Tutort/Leetcode_02_baseball_game.py
@@ -3,3 +3,25 @@
 https://leetcode.com/problems/baseball-game/description/
 '''
 
+import math
+class Solution:
+    def is_convertible_to_number(self, text: str)->bool:
+        try:
+            return math.isfinite(float(text)) #True/False
+        except ValueError:
+            return False
+
+    def calPoints(self, operations: List[str]) -> int:
+        record=[]
+        for op in operations:
+            if self.is_convertible_to_number(op):
+                record.append(int(op))
+            elif op=="C":
+                record.pop()
+            elif op=="D":
+                record.append(record[-1]*2)
+            elif op=="+":
+                record.append(record[-1]+record[-2])
+        return sum(record)
+
+
