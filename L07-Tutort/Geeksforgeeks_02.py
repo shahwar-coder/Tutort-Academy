e5d01f2bfe0d7c29c4d8bd88ee312ae391e6221f
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-29 21:21:45 +0530

    Update Geeksforgeeks_02.py

--- L07-Tutort/Geeksforgeeks_02.py
+++ L07-Tutort/Geeksforgeeks_02.py
@@ -5,23 +5,26 @@ https://www.geeksforgeeks.org/problems/wave-array-1587115621/1
 
 from typing import List
 class Solution:
-    def swap(self, arr: List[int], index: int)->None:
+    def _swap(self, arr: List[int], index: int)->None:
         """
-        I just swap two elements
+        I just swap two elements.
+        In Place manipulation, no returning
         """
-        temp=arr[index]
-        arr[index]=arr[index+1]
-        arr[index+1]=temp
+        arr[index], arr[index+1] = arr[index+1], arr[index]
         
         
     def sortInWave(self, arr):
-        if len(arr)==1:
+        if len(arr)<=1:
             return arr
         for index in range(len(arr)-1):
             if index%2==0:
                 if arr[index]<arr[index+1]:
-                    self.swap(arr, index)
+                    self._swap(arr, index)
             else:
                 if arr[index]>arr[index+1]:
-                    self.swap(arr, index)
+                    self._swap(arr, index)
         return arr
+        
+obj=Solution()
+result = obj.sortInWave(arr=[1,2,3,4,5])
+print(result)
