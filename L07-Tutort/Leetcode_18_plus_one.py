11f99ceed648661059a84eedb1a7fdedfb990012
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-31 03:53:21 +0530

    Update Leetcode_18_plus_one.py

--- L07-Tutort/Leetcode_18_plus_one.py
+++ L07-Tutort/Leetcode_18_plus_one.py
@@ -9,3 +9,22 @@ class Solution:
         number = int(''.join(map(str, digits)))
         number+=1
         return list(map(int, list(str(number))))
+
+
+# =========================== Below can be used as well, carry based, benefit, used O(1) space not O(n) like in above case ===================================
+
+# from typing import List
+
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         """
+#         Increment the number represented by digits by one, returning a new list of digits.
+#         Works in O(n) time and O(1) extra space.
+#         """
+#         for i in range(len(digits) - 1, -1, -1):
+#             if digits[i] < 9:
+#                 digits[i] += 1
+#                 return digits
+#             digits[i] = 0
+#         return [1] + digits
+
