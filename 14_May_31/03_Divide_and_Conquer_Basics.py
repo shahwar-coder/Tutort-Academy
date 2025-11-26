a776604f21f3460200e8eeb0c6284224c53b09a3
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-27 00:06:07 +0530

    Create 03_Divide_and_Conquer_Basics.py

---
+++ 14_May_31/03_Divide_and_Conquer_Basics.py
@@ -0,0 +1,52 @@
+'''
+Q1. What does "Divide & Conquer" mean in simple words?
+Ans:
+It means breaking a big problem into smaller parts, solving them, and joining the answers.
+'''
+# Example: To find the biggest number → split list in half → find biggest in each half → compare.
+
+
+
+'''
+Q2. What are the three steps of Divide & Conquer?
+Ans:
+Divide → Conquer → Combine.
+'''
+# Example: Split a class into two groups → check each group → decide final winner.
+
+
+
+'''
+Q3. Why do we divide a problem into smaller parts?
+Ans:
+Smaller problems are easier and faster to solve than one big problem.
+'''
+# Example: Cleaning a big room is easier if two people clean two halves.
+
+
+
+'''
+Q4. Give one real-life example of Divide & Conquer.
+Ans:
+Searching a name in a dictionary – open the middle, pick the correct half, repeat.
+'''
+# Example: Searching “S” in alphabetical pages.
+
+
+
+'''
+Q5. Does Divide & Conquer always mean recursion?
+Ans:
+Mostly yes, because solving smaller problems repeatedly fits recursion naturally.
+'''
+# Example: max(left) and max(right) → return max of both.
+
+
+'''
+Q6. Why is Divide & Conquer useful?
+Ans:
+It reduces work, increases speed, and breaks complex tasks into simple ones.
+'''
+# Example: Merge sort is faster than normal sorting because of splitting.
+
+
